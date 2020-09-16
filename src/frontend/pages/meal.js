55c899188bc8f86f0c397b0d8acb62c6fa99fbb8
Maher <maherhussain6@gmail.com>
2020-09-16 11:30:07 +0000

    meake frontend

--- src/frontend/pages/meal.js
+++ src/frontend/pages/meal.js
@@ -1,4 +1,15 @@
 window.handleMealRequest = params => {
   document.body.innerHTML = `
   <h1>Meal with id ${params.id}</h1>`;
+
+
+  const api = `api/meals/${params.id}`;
+  fetch(api)
+    .then((res) => res.json())
+    .then((result) => renderResult(result));
+
+    function renderResult(meal) {
+      document.body.innerHTML = `<p>meal title : ${meal.title} <br>
+   meal description : ${meal.description}</p>`;
+    }
 };
--- src/frontend/pages/meals.js
+++ src/frontend/pages/meals.js
@@ -1,9 +1,34 @@
 window.handleMealsRequest = () => {
   document.body.innerHTML = `
-  <h1>Meals</h1>`;
+  <h1>Meals</h1>
+  
+   <div id="meals">
+        <table id="table">
+            <tbody>
+                <tr>
+                    <th> meal title</th>
+                    <th>description</th>
+                </tr>
+
+            </tbody>
+        </table>
+
+    </div>`;
 
   // make sure the backend api works before working with it here
   fetch("/api/meals")
-    .then(response => response.json())
-    .then(meals => console.log(meals));
+    .then((response) => response.json())
+    .then((meals) => renderMeals(meals));
+
+
+    function renderMeals(meals) {
+      const table = document.querySelector("#table");
+      const tbody = document.querySelector("tbody");
+      meals.forEach((meal) => {
+        tbody.innerHTML += `<tr>
+      <td>${meal.title}</td>
+      <td> ${meal.description}</td>
+      </tr>`;
+      });
+    }
 };

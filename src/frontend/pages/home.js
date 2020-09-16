6e27a71d1a5e1b715f97f6d6d3c2ebd056d69513
Maher <maherhussain6@gmail.com>
2020-09-16 11:33:56 +0000

    adding style

--- src/frontend/pages/home.js
+++ src/frontend/pages/home.js
@@ -1,5 +1,11 @@
 window.handleHomeRequest = () => {
-  document.body.innerHTML = `<h1>Home</h1>
+  document.body.innerHTML = ` <header>
+  <h4>Welcome to meal-sharing applicaion -</h4>
+  <p>this is my first app using node js - </p>
+</header>
+
+
+  <h1>Home</h1>
   <a href="meals" data-navigo>Meals</a>
   asd
   <a href="meal/1" data-navigo>meal/1</a>
